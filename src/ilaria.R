2065690939bb7b9b74e382bd46d9b537a5e4c214
Affolter  Ilaria <ailaria@ethz.ch>
2025-05-26 13:25:23 +0200

    change name to ilaria

--- src/ilaria.R
+++ src/ilaria.R
@@ -10,7 +10,7 @@
 
 hello_lab <- function() {
   # TODO: Replace this with your name
-  name <- "CHANGE_ME"
+  name <- "ilaria"
 
   # TODO: Optionally change what this does
   message <- paste("👋 Hello from", name, "!")
