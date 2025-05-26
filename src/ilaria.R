4c9a1796476c2a39ede209e0195e4ed2bd3b4fac
Elena Krismer <70535771+elena-krismer@users.noreply.github.com>
2025-05-26 07:05:39 +0200

    add scripts

---
+++ src/ilaria.R
@@ -0,0 +1,27 @@
+#' Hello Lab Function
+#'
+#' A simple function template for Git training.
+#' Each person should modify the function to print their name
+#' and optionally do something extra (e.g., show a random number, favorite R trick, etc.)
+#'
+#' @return A custom message
+#' @export
+#' @author Your Name
+
+hello_lab <- function() {
+  # TODO: Replace this with your name
+  name <- "CHANGE_ME"
+
+  # TODO: Optionally change what this does
+  message <- paste("👋 Hello from", name, "!")
+
+  # Extra: Add your own flavor (e.g., a quote, random number, emoji, whatever)
+  extra <- "This is the default message. Customize me!"
+
+  # Print output
+  cat(message, "\n")
+  cat(extra, "\n")
+}
+
+# Example call
+# hello_lab()
